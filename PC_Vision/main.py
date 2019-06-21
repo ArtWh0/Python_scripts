5020c86e848606942187f96aee85cafa7c542722
ArtWh0 <artwh0@yandex.ru>
2019-06-21 16:16:30 +0300

    Replace 0 and 1

--- PC_Vision/main.py
+++ PC_Vision/main.py
@@ -1,16 +1,23 @@
 def view_bytes(fpath, delim=''):
 	ret=[]
 	i=1
+	new=''
 	with open(fpath, 'br') as f:
 		for chunk in iter(lambda: f.read(20),b''):			
 			ret.append(delim.join([f'{byte:08b}' for byte in chunk]))
 			i=i+1
 	for up_el in ret:
+		print('Work with element # '+str(ret.index(up_el)))
 		for down_el in up_el:
 			if down_el=='0':
-				ret[ret.index(up_el)][up_el.index(down_el)]=' '
+				
+				new=new+' '				
 			else:
-				ret[ret.index(up_el)][up_el.index(down_el)]='*'
+				
+				new=new+'*'				
+		print('New element '+new+' on position'+str(ret.index(up_el)))
+		ret.insert(ret.index(up_el),new)
+		new=''
 	return ret
  
 
