c97a898009c0db2e98aa8abafeae0df18430fcf2
ArtWh0 <artwh0@yandex.ru>
2019-06-21 16:25:23 +0300

    Little fix

--- PC_Vision/main.py
+++ PC_Vision/main.py
@@ -4,17 +4,20 @@ def view_bytes(fpath, delim=''):
 	new=''
 	with open(fpath, 'br') as f:
 		for chunk in iter(lambda: f.read(20),b''):			
-			ret.append(delim.join([f'{byte:08b}' for byte in chunk]))
-			i=i+1
+			ret.append(delim.join([f'{byte:08b}' for byte in chunk]))			
 	for up_el in ret:
 		print('Work with element # '+str(ret.index(up_el)))
 		for down_el in up_el:
-			if down_el=='0':
-				
+			if down_el=='0':				
 				new=new+' '				
 			else:
 				
-				new=new+'*'				
+				new=new+'*'
+		if new==ret[ret.index(up_el)-1]:
+			i=i+1
+		if i>5:
+			i=0
+			continue				
 		print('New element '+new+' on position'+str(ret.index(up_el)))
 		ret.insert(ret.index(up_el),new)
 		new=''
